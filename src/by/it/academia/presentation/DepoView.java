51668757b85c0861eb3aedf5c34653259b2ff8f8
hellwere <hellwere@mail.ru>
2021-04-21 10:16:18 +0300

    Удаление не используемого метода

--- src/by/it/academia/presentation/DepoView.java
+++ src/by/it/academia/presentation/DepoView.java
@@ -1,6 +1,7 @@
 package by.it.academia.presentation;
 
 import by.it.academia.entity.Depo;
+import by.it.academia.entity.DepoLogic;
 import by.it.academia.entity.Train;
 
 import java.text.DateFormat;
@@ -11,7 +12,7 @@ import java.util.Comparator;
 public class DepoView {
     public static void depoPrinter(Depo depo) {
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        System.out.println("Сртировка по времени отправки");
+        System.out.println("сортировка по времени отправки");
         System.out.println("Поезд:          Дата:              Точка назначения:");
         Collections.sort(depo.getTrains(), Comparator.comparing(Train::getDispatchDataTime));
         for (Train tr : depo.getTrains()) {
@@ -22,7 +23,7 @@ public class DepoView {
 
     public static void depoPrinterRouteTarget(Depo depo) {
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        System.out.println("Сртировка по месту доставки");
+        System.out.println("сортировка по месту доставки");
         System.out.println("Поезд:          Дата:              Точка назначения:");
         Collections.sort(depo.getTrains(), Comparator.comparing(Train::getRoadTarget));
         for (Train tr : depo.getTrains()) {
@@ -40,5 +41,22 @@ public class DepoView {
                 System.out.printf("%s | %s | %s\n", tr.getTrainNumber(), df.format(tr.getDispatchDataTime().getTime()), tr.getRoadTarget());
             }
         }
+    }
+
+    public static void depoHandPrinterRouteTarget(Depo depo) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        System.out.println("Ручная сортировка по месту доставки");
+        for (Train tr : DepoLogic.heapSort(depo.getTrains(), true)) {
+            System.out.printf("%s | %s | %s\n", tr.getTrainNumber(), df.format(tr.getDispatchDataTime().getTime()), tr.getRoadTarget());
+        }
+    }
+
+    public static void depoHandPrinterNumber(Depo depo) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        System.out.println("Ручная сортировка по номеру поезда");
+        for (Train tr : DepoLogic.heapSort(depo.getTrains(), false)) {
+            System.out.printf("%s | %s | %s\n", tr.getTrainNumber(), df.format(tr.getDispatchDataTime().getTime()), tr.getRoadTarget());
+        }
+
     }
 }
